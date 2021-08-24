15f52c28a4b1e5d3c489d069d8a1d32a6d26b813
David Folkins <davidf@EXS-Programmer11.local>
2021-08-24 14:53:06 -0400

    add more boiler plate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
+import AddShoppingCart from '@material-ui/icons/AddShoppingCart';
+import { Wrapper } from './App.styles';
+export type CartItemType = {
+  id: number;
+  category: string;
+  descriptions: string;
+  image: string,
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 function App() {
+
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  )
+  
+  console.log(data)
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />
+
+  if (error) return <div>Something Went Wrong...</div>
+  
   return (
     <div className="App">
       Shopping Cart
